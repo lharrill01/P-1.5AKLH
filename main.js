0d2e2d653815ddf6484781980392e579fbafac8e
Leroy Harrill <lharrill01@gmail.com>
2019-05-31 13:05:21 -0400

    update files

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+let btntest = document.getElementById("btntest");
+btnc.addEventListener("click", textDisappear);
+
+function textDisappear(){
+    btntest.getElementsByClassName("con").visibility = hidden;
+}
