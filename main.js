89f069b1399f84fea0a9694e72cc6cb7ede2a231
Leroy Harrill <lharrill01@gmail.com>
2019-05-31 13:10:05 -0400

    update files

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 'use strict';
 
 
-let btntest = document.getElementById("btntest");
-btnc.addEventListener("click", textDisappear);
+let btntest = document.getElementById("btntest").addEventListener("click", textDisappear);
 
 function textDisappear(){
-    btntest.getElementsByClassName("con").visibility = hidden;
+    document.getElementsByClassName("con").visibility = hidden;
 }
